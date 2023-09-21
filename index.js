69268939ba338dd41dfebf55d9eb795278b5c58e
sandu <badanausandu@gmail.com>
2023-09-21 23:23:01 +0300

    commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var variabila = 1
+var variabila2 = 2
+var variabila3 = 3
+
+
+
+const numb = 44
+const str = 'un string'
+const bool = false
+const undef = undefined
+const faraValoare = null
+const obj = {}
+let obj1 = {
+    nume: "Badanau",
+    prenume: "Alexandru",
+    hobby: "Masini, it, tech",
+    varsta: "17",
+    Ocupatie: "elev",
+}
+console.log(variabila + variabila2 + variabila3)
+console.log(variabila == variabila2 == variabila3)
+console.log(variabila > variabila2 > variabila3)
+console.log(variabila >= variabila2 >= variabila3)
+console.log(variabila < variabila2 < variabila3)
+console.log(variabila <= variabila2 <= variabila3)
+console.log(typeof numb)
+console.log(typeof str)
+console.log(typeof bool)
+console.log(typeof undef)
+console.log(typeof faraValoare)
+console.log(typeof obj)
+console.log('Hello World')
+console.log(obj1)
